5547d34701e462a7489c77bb2ad1a92f0460a407
Jose21NC <josealonzo121@gmail.com>
2025-10-26 02:38:43 -0600

    build(web): actualizar dist tras cambios de auth/UI\n\n- Regenerado dist con Vite\n- Actualización de bundles y index.html

--- dist/assets/web-0GQKigtO.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-Cmr5eQhQ.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ dist/assets/web-DJLvpBD2.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BO30kwfz.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
